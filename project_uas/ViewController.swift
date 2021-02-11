ee60c43189d64e510eeba41143866f9485475ab0
garrystevens007 <49149545+garrystevens007@users.noreply.github.com>
2021-02-11 13:43:16 +0700

    Initial Commit

---
+++ project_uas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project_uas
+//
+//  Created by Garry on 11/02/21.
+//  Copyright © 2021 Garry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
